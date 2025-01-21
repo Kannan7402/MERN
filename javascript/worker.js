ff58fc5d4add92a82da709ab8df9f4a6f0f52872
Kannan Selvaraj <kannan.selvaraj@aspiresys.com>
2025-01-21 18:33:20 +0530

    21 Jan

--- javascript/worker.js
+++ javascript/worker.js
@@ -1,3 +1,5 @@
-onmessage = function(e) {
-    postMessage('Received: ' + e.data); // Send response back to main thread
-};
+// onmessage = function(e) {
+//     postMessage('Received: ' + e.data); // Send response back to main thread
+// };
+"use strict";
+x = 3.14;  
