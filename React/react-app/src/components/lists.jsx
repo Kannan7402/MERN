ed9b79600f0fe45a333a182636d8d35fadd8ce78
Kannan7402 <kannan.selvaraj@aspiresys.com>
2025-02-21 20:02:55 +0530

    21 Feb

--- React/react-app/src/components/lists.jsx
+++ React/react-app/src/components/lists.jsx
@@ -3,13 +3,15 @@ function List ()
 {
     const persons = [{name:'kannan',age:22},
         {name:'bala',age:22},
-        {name:'rathesh',age:23}
+        {name:'rathesh',age:23},
+        {name:'kannan',age:22}
     ];
+    console.log(persons.includes('kannan'))
     return(
         <ul>
             {persons.map((person,index)=>
             (
-                <li key = {index}>{person.name} & {person.age}</li>
+                <li key = {index}>{person.name}</li>
             )
             )}
         </ul>
